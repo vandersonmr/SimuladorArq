b78bff4725314949311956e09976f9278e3823a8
Vanderson M. do Rosario <vandersonmr2@gmail.com>
2012-09-02 20:05:55 -0300

    Tudo com Linux Style

--- src/banco.c
+++ src/banco.c
@@ -5,34 +5,40 @@ int32 pc;
 int32 registers[32];
 
 /* inicializacao */
-void BANCO_Inicializacao() {
- for(int x=0;x<32;registers[x++] = 0); 
- pc = 0;
+void BANCO_Inicializacao()
+{
+	for (int x = 0; x < 32; registers[x++] = 0) ;
+	pc = 0;
 }
 
 /* finalizacao */
-void BANCO_Finalizacao() {
+void BANCO_Finalizacao()
+{
 
 }
 
 /* armazena dados no resgistrador */
-void BANCO_SetRegister (int32 address, int32 data) {
-  if (address>=0)
-    registers[address] = data; 
+void BANCO_SetRegister(int32 address, int32 data)
+{
+	if (address >= 0)
+		registers[address] = data;
 }
- 
-/* retorna dado de um determinado registrador */
-int32 BANCO_GetRegister (int32 address) {
 
- return registers[address];
+/* retorna dado de um determinado registrador */
+int32 BANCO_GetRegister(int32 address)
+{
+
+	return registers[address];
 }
 
 /* armazena dado */
-void BANCO_SetPc () {
+void BANCO_SetPc()
+{
 
 }
 
 /* retorna dado */
-int BANCO_GetPc() {
+int BANCO_GetPc()
+{
 
 }
