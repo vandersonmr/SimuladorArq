9bff494d272195b9d3736ce3d283551546fdbd15
root <root@vanderson-PC.(none)>
2012-10-30 10:42:20 -0200

    ULA pronta
    Committer: vanderson M. R. (vandersonmr2@gmail.com)
    	new file:   .ula.c.swp
    	modified:   ula.c

--- src/ula.c
+++ src/ula.c
@@ -1,14 +1,27 @@
 
 /* executa a operacao Tarefa entre os operandos A e B */
-int32 ULA_execute (int32 A, int32 B, uint32 Tarefa){ 
-  int32 Local = 0;
+int32 ULA_execute (int32 A, int32 B, uint32 tarefa){ 
+  int32 local = 0;
   
-  switch (Tarefa) {
-  case 0  : Local = A + B; break;                      /* soma */
-  case 1  : Local = (unsigned)A + (unsigned)B; break;  /* soma sem sinal */
-
+  switch (tarefa) {
+	case 0 : local = A + B; break; 
+ 	case 1 : local = (unsigned)A + (unsigned)B; break;
+	case 2 : local = A - B; break;
+	case 3 : local = (unsigned)A - (unsigned)B; break;
+	//case 4 : local = A * B; break; Pode ter * na ULA?
+	//case 5 : local = (unsigned)A * (unsigned)B; break;
+	case 6 : local = A & B; break;
+        case 7 : local = A | B; break;	
+	case 8 : local = A ^ B; break;
+	case 9 : local = ~(A & B); break;
+	case 10: local = ~(A | B); break;
+	case 11: local = ~(A ^ B); break;
+	case 12: local = ~(A); break;
+	case 13: local = A  << B; 
+	case 14: local = A >> B;
+	// !TODO ASR
   }
  
-  return (Local);
+  return (local);
 
 }
